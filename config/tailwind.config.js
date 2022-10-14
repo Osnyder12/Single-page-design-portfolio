0371ba48dbfa9e20e177fba024860366c76637c4
Osnyder12 <snyderowen12@gmail.com>
2022-10-14 14:29:06 -0400

    new section

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,8 +13,15 @@ module.exports = {
     },
     extend: {
       colors: {
-        'background': '#FFF7F0',
-        'galactic-blue': '#755CDE'
+        'galactic-blue': '#755cde',
+        'summer-yellow': '#f6a560',
+        'salmon': '#f39e9e',
+        'light-red': '#eb7565',
+        'cyan': '#61c4b7',
+        'deep-purple': '#552049',
+        'brand-black': '#030303',
+        'medium-brown': '#7a746e',
+        'light-cream': '#fff7f0'
       },
     },
   },
